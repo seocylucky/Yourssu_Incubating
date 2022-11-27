ec3e55805f56e4aaf52124e327ab218901960a19
seocylucky <seocylucky@github.com>
2022-11-27 22:37:36 +0900

    2차 과제ing🥲

--- 2Week/src/index.ts
+++ 2Week/src/index.ts
@@ -1,1 +1,104 @@
-const list = document.getElementById('item-list') as HTMLUListElement | null;
+// const toDoForm = document.querySelector<HTMLFormElement>(".todo-form");
+// const toDoList = document.querySelector<HTMLUListElement>('.item-list');
+
+// type Todo = {
+//     id?: number
+//     item?: string
+//     status?: "DONE" | "NOT_DONE"
+// }
+
+// let taskInput = document.querySelector<HTMLInputElement>("new-task-input");
+
+// toDoForm?.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     let toDoInput = taskInput.value;
+//     if (toDoInput !== '') {
+//         addTask(toDoInput);
+//         toDoInput = '';
+//         const InputTodo: Todo = {
+//             item: toDoInput,
+//             status: "NOT_DONE",
+//         }
+
+//         fetch("/todo", {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application-json",
+//             },
+//             body: JSON.stringify(InputTodo),
+//         })
+//         .then(res => res.json())
+//         .then(res => console.log(res)
+//     )}
+//     fetch('/todo', {
+//         method: "GET",
+//         headers: {
+//             "Content-Type": "application-json",
+//         },
+//         })
+//         .then(res => res.json())
+//         .then(res => console.log(res)
+//         )
+// })
+
+// function addTask(value: string) {
+//     let ul = document.querySelector<HTMLUListElement>('ul');
+//     let li = document.createElement('li');
+//     li.innerHTML = `<li class="item"><label for="1">${value}<span class="cancle">&times;</span></label><input id="1" type="checkbox" checked/></li>`;
+//     ul.appendChild(li);
+//     document.querySelector<HTMLUListElement>('item-list').style.display = 'block';
+// }
+
+// toDoList?.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     const id = document.querySelectorAll("item-list.li").length+1;
+//     const target = e.target as HTMLUListElement
+//     if(target.className == 'cancle') {
+//         toDoList.addEventListener('click', (e) => {
+//             e.preventDefault();
+//             const deleteTodo: Todo = {
+//                 id: id
+//             }
+        
+//             fetch("/todo", {
+//                 method: "DELETE",
+//                 headers: {
+//                     "Content-Type": "application-json",
+//                 },
+//                 body: JSON.stringify(deleteTodo),
+//             })
+//             .then(res => res.json())
+//             .then(res => console.log(res)
+//         )  
+//             let remove = target.parentNode;
+//             let parent = remove.parentNode;
+//             parent.removeChild(remove);
+//         })
+//     }
+//     else {
+//         toDoList.addEventListener('click', (e) => {
+//             e.preventDefault();
+//             let toDoInput = taskInput.value;
+//             const Checkinput = document.querySelector<HTMLInputElement>("checkbox");
+        
+//             const CheckTodo: Todo = {
+//                 id: id,
+//                 item: toDoInput,
+//                 status: "DONE"
+//             }
+            
+//             fetch("/todo", {
+//                 method: "PATCH",
+//                 headers: {
+//                     "Content-Type": "application-json",
+//                 },
+//                 body: JSON.stringify(CheckTodo),
+//             })
+//             .then(res => res.json())
+//             .then((res) => {
+//                 Checkinput.checked = true;
+//                 console.log(res)}
+//         )  
+//         })
+//     }
+// })
--- 2Week/src/main.ts
+++ 2Week/src/main.ts
@@ -4,31 +4,136 @@ import { worker } from './mocks/browser'
 
 worker.start({ onUnhandledRequest: 'bypass' })
 
-
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <main>
     <div class="wrapper">
       <h1 class="title">Yourssu Todo List</h1>
-      <form class="toto-form">
-        <input placeholder="Write Your tood.." />
-        <button type="button">Submit</button>
+      <form class="todo-form">
+        <input placeholder="Write Your todo.." class="new-task-input"/>
+        <button class="submitBtn">Submit</button>
       </form>
       <ul class="item-list">
-          <li class="item">
-            <label for="1">
-              TODO1
-              <span class="cancle" onClick="alert('Hello!');">&times;</span>
-            </label>
-            <input id="1" type="checkbox" checked/>
-          </li>
-          <li class="item">
-            <label for="2">
-              TODO2
-              <span class="cancle">&times;</span>
-            </label>
-            <input id="2" type="checkbox"/>
-          </li>
-        </ul>
+      </ul>
     </div>
   </main>
 `
+
+const toDoForm = document.querySelector<HTMLFormElement>(".todo-form");
+const toDoList = document.querySelector<HTMLUListElement>('.item-list');
+
+type Todo = {
+    id?: number
+    item?: string
+    status?: "DONE" | "NOT_DONE"
+}
+
+const taskInput = document.querySelector<HTMLInputElement>(".new-task-input");
+
+toDoForm?.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    let toDoInput = taskInput?.value;
+    if (toDoInput !== '') {
+        addTask(toDoInput);
+        const InputTodo: Todo = {
+            item: toDoInput,
+            status: "NOT_DONE",
+        }
+
+        await fetch("/todo", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application-json",
+            },
+            body: JSON.stringify(InputTodo),
+        })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res)
+            taskInput.value = '';
+        }
+    )}
+    await fetch('/todo', {
+        method: "GET",
+        headers: {
+            "Content-Type": "application-json",
+        },
+        })
+        .then(res => res.json())
+        .then(res => console.log(res)
+        )
+})
+
+function addTask(value: String) {
+    // let ul = document.querySelector<HTMLUListElement>('ul');
+    let li = document.createElement('li');
+    li.innerHTML = `<li class="item"><label>${value}<span class="cancle">&times;</span></label><input class="check-Box" type="checkbox"></li>`;
+    toDoList?.append(li);
+
+    const id = document.querySelectorAll(".item-list.li").length+1;
+    const delBtn = document.querySelector<HTMLButtonElement>(".cancle");
+    const Checkinput = document.querySelector<HTMLInputElement>(".check-Box");
+    const toDoInput = taskInput!.value;
+
+    const deleteTodo: Todo = {
+        id: id
+    }
+
+    delBtn.addEventListener('click', async (e) => {
+        e.preventDefault();
+    
+        await fetch("/todo", {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application-json",
+            },
+            body: JSON.stringify(deleteTodo),
+        })
+        .then(res => res.json())
+        .then(res => {
+            li.remove();
+            console.log(res)
+            console.log('삭제 완료')
+        })
+        await fetch('/todo', {
+            method: "GET",
+            headers: {
+                "Content-Type": "application-json",
+            },
+            })
+            .then(res => res.json())
+            .then(res => console.log(res)
+            )  
+    })
+
+
+    const CheckTodo: Todo = {
+        id: id,
+        item: toDoInput,
+        status: "DONE"
+    }
+    
+    Checkinput.addEventListener('click', async (e) => {
+        e.preventDefault();
+        
+        await fetch("/todo", {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application-json",
+            },
+            body: JSON.stringify(CheckTodo),
+        })
+        .then(res => res.json())
+        .then((res) => {
+            Checkinput.checked = true;
+            console.log(res)}
+    ) 
+    await fetch('/todo', {
+        method: "GET",
+        headers: {
+            "Content-Type": "application-json",
+        },
+        })
+        .then(res => res.json())
+        .then(res => console.log(res)
+        ) 
+})}
