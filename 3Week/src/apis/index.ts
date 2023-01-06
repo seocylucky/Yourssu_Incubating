27aa81c81329cbd7c9294492eef357ba1156cbf8
seocylucky <seocylucky@github.com>
2023-01-07 03:11:47 +0900

    fix: axios 오류 수정

--- 3Week/src/apis/index.ts
+++ 3Week/src/apis/index.ts
@@ -3,42 +3,46 @@ import { useRecoilState } from "recoil";
 import { TodoTypes, todosState } from '../recoil/todo';
 
 export const requestPost = async (item: string, status: string) => {
-    return await axios
-    .post("/todo", {
+    return axios({
+        method: "POST",
+        url: `/todo`,
         headers: {
             "Content-Type": "application-json",
         },
         data: {
             item: item,
             status: status,
-        }
-    })
+        },
+    });
 }
 
 export const requestGetTodo = async () => {
-    return await axios
-    .get("/todo", {
+    return axios({
+        method: "GET",
+        url: `/todo`,
         headers: {
             "Content-Type": "application-json",
         },
-    })
+    });3
 }
 
 export const requestDeleteTodo = async (id: string) => {
-    return await axios
-    .delete("/todo", {
+    return axios({
+        method: "DELETE",
+        url: `/todo`,
         headers: {
             "Content-Type": "application-json",
         },
         data: {
-            id
-        }
-    })
+            id,
+        },
+    });
 }
 
 export const requestPatchTodo = async (id: string, item: string) => {
-    return await axios
-    .patch("/todo", {
+    return axios({
+        method: "PATCH",
+        url: `/todo`,
         headers: {
             "Content-Type": "application-json",
         },
@@ -46,6 +50,6 @@ export const requestPatchTodo = async (id: string, item: string) => {
             id,
             item,
             status: "DONE"
-        }
-    })
+        },
+    });3
 }
